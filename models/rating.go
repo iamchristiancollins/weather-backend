958469f84fa28d99db8bafdcb9192c67f3dcc017
iamchristiancollins <105734903+iamchristiancollins@users.noreply.github.com>
2024-10-09 16:33:55 -0500

    start controll dir

--- models/rating.go
+++ models/rating.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Rating struct {
 	gorm.Model
 	UserID       uint   `gorm:"not null"`
-	Temperature  int    `gorm: "not null"`
+	Temperature  int    `gorm:"not null"`
 	TopRating    string `gorm:"not null"`
 	BottomRating string `gorm:"not null"`
 }
