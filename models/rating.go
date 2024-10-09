eb66cd47de9d5f53189080fafe2a9c7e1bdc260f
iamchristiancollins <105734903+iamchristiancollins@users.noreply.github.com>
2024-10-09 16:28:50 -0500

    add rating model

---
+++ models/rating.go
@@ -0,0 +1,11 @@
+package models
+
+import "gorm.io/gorm"
+
+type Rating struct {
+	gorm.Model
+	UserID       uint   `gorm:"not null"`
+	Temperature  int    `gorm: "not null"`
+	TopRating    string `gorm:"not null"`
+	BottomRating string `gorm:"not null"`
+}
